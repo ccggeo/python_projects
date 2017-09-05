d4f538fc9e4baa1d0381007d51dc203a82762ad0
ccggeo <georgeconnorc@gmail.com>
2017-09-05 16:46:29 +0100

    function and if statement input

--- password-generator.py
+++ password-generator.py
@@ -1,13 +1,32 @@
 #!/bin/python3
 import random
 
-chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz1234567890'
-password = ''
-
-range_input = int(input("How long do you want your password to be ?: "))
-
-for c in range(range_input):
-	password += random.choice(chars)
-print (password)
+def genpass():
 
 
+		chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz1234567890'
+		password = ''
+
+		range_input = int(input("How long do you want your password to be ?: "))
+
+		for c in range(range_input):
+			password += random.choice(chars)
+		print (password)
+	
+		prompt = input("Repeat? [Y] or [N]")
+
+
+		print (prompt)
+		
+genpass()	
+		
+		
+		
+		
+		
+		
+if (prompt is 'Y'):	
+	genpass()
+	
+else:
+	print("finished")
