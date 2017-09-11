65ce3f8c79e4e34ff5507267cbd444dda02cabd5
ccggeo <georgeconnorc@gmail.com>
2017-09-11 15:00:29 +0100

    copy folders and subdirectories using copy_tree

---
+++ copy_directory.py
@@ -0,0 +1,7 @@
+from distutils.dir_util import copy_tree
+
+# copy subdirectory. Uses copy_tree
+fromDir = "/Volumes/CCSA_X64FRE_EN-US_DV5"
+toDir = "/Users/cgeorge/desktop/ISO"
+
+copy_tree(fromDir, toDir)
