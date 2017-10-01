e42703abca9bb8ee44f4befc9abbf9721f96e948
Connor George <georgeconnorc@gmail.com>
2017-10-01 12:16:17 +0100

    Tracks the ISS with live data

---
+++ ISS_tracker.py
@@ -0,0 +1,19 @@
+#http://api.open-notify.org/iss-now.json
+import urllib.request
+import json
+
+response = urllib.request.urlopen('http://api.open-notify.org/iss-now.json').read()
+json_obj = str(response, 'utf-8')
+
+data = json.loads(json_obj)
+
+time = data['timestamp']
+timestamp = str(time)
+
+position = data['iss_position']
+
+longitude = position['longitude']
+latitude = position['latitude']
+print('The longitudial position of the ISS is currently: ' + longitude )
+print('The longitudial position of the ISS is currently: ' + latitude)
+print('Timestamp: ' + timestamp)
