ca95723cef557e492eed426e89a807235484be0e
Connor George <georgeconnorc@gmail.com>
2017-10-14 18:41:17 +0100

    Investigation into different geocode apis

--- ISS_tracker.py
+++ ISS_tracker.py
@@ -1,8 +1,11 @@
-import urllib.request
+import urllib2
 import json
+import geocoder
+#pip install future
+#pip install geocoder
 
-response = urllib.request.urlopen('http://api.open-notify.org/iss-now.json').read()
-json_obj = str(response, 'utf-8')
+response = urllib2.urlopen('http://api.open-notify.org/iss-now.json').read()
+json_obj = str(response)
 
 data = json.loads(json_obj)
 
@@ -16,3 +19,28 @@ latitude = position['latitude']
 print('The latitudial position of the ISS is currently: ' + latitude)
 print('The longitudial position of the ISS is currently: ' + longitude )
 print('Timestamp: ' + timestamp)
+
+
+
+#http://www.geonames.org/export/web-services.html
+#https://stackoverflow.com/questions/19590584/how-to-find-which-ocean-a-coordinate-is-on
+
+
+g = geocoder.google([latitude, longitude], method='reverse')
+#print(g.country_long)
+
+g.city
+g.state
+g.state_long
+g.country
+g.country_long
+
+
+
+###
+geonames = urllib2.urlopen('http://api.geonames.org/findNearbyPlaceNameJSON?lat=47.3&lng=9&username=iMHx3RncHX').read()
+_json_obj = str(geonames)
+
+_data = json.loads(_json_obj)
+
+geonamesData = _data['geonames']
