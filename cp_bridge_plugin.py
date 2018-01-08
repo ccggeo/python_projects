4e4cd9a21d18ac46883725d41ef7f1cf36ce5258
ccggeo <georgeconnorc@gmail.com>
2018-01-08 17:29:59 +0000

    subprocess copy

---
+++ cp_bridge_plugin.py
@@ -0,0 +1,37 @@
+
+
+import subprocess
+
+MACS = "I"    
+
+
+
+
+for m in MACS:
+    print(m)
+    subprocess.Popen(["cp", "/home/cgeorge/Desktop/tmp", "" shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+ #  scp -r /Users/cgeorge/Desktop/tmp admin@HOSTNAME:/tmp
+
+
+
+
+
+
+
+
+
+#subprocess.call("scp -r /Users/cgeorge/Desktop/tmp admin@"MACS":/tmp", shell=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
