7069f41eeb3ef15c2bf4d65c9136a10d610abec5
ccggeo <georgeconnorc@gmail.com>
2018-01-31 12:12:05 +0000

    For statement

--- invoke-command-remotely.py
+++ invoke-command-remotely.py
@@ -8,15 +8,16 @@ def invokecommand(TARGET, USER, COMMAND):
         #command to run on host
     #https://stackoverflow.com/questions/7468668/python-subprocess-readlines
     ssh = subprocess.Popen(["ssh", "%s" % x, COMMAND], shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     result = ssh.stdout.readlines()
     if result == []:
         error = ssh.stderr.readlines()
         print >>sys.stderr, "ERROR: %s" % error
     else:
-        print result
+        for x in result:
+            print(x)
+#        print result
 
-USER =  raw_input(['USERNAME OF USER'])
+USER =   raw_input(['USERNAME OF USER'])
 TARGET = raw_input(['USERNAME OF Target'])
 COMMAND =  raw_input(['USERNAME OF Command'])
 
