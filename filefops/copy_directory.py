6452969682524c81f8f69e526d4d2261cbf65958
ccggeo <georgeconnorc@gmail.com>
2018-03-09 17:37:46 +0000

    remove incorrect directory name

--- filefops/copy_directory.py
+++
@@ -1,9 +0,0 @@
-from distutils.dir_util import copy_tree
-
-# copy subdirectory. Uses copy_tree
-fromDir = "/Volumes/CCSA_X64FRE_EN-US_DV5"
-toDir = "/Users/cgeorge/desktop/ISO"
-copy_tree(fromDir, toDir)
-
-
-#http://pythoncentral.io/how-to-movecopy-a-file-or-directory-folder-with-a-progress-bar-in-python/
