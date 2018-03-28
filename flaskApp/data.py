25d97e406a43886c4057eb6cceca063b3cecf601
Connor George <georgeconnorc@gmail.com>
2018-03-28 17:15:14 +0100

    first FlaskApp Commit

---
+++ flaskApp/data.py
@@ -0,0 +1,60 @@
+def Articles():
+
+    articles =[
+        {
+            'id': 1,
+            'title':'Article one',
+            'body': 'Ipsum',
+            'author': 'Jesus',
+            'create_date': '25-12-2017'
+        },
+        {
+             'id': 2,
+            'title': 'Article two',
+            'body': 'Lorem',
+            'author': 'John',
+            'create_date': '28-12-2017'
+
+        },
+        {
+            'id': 3,
+            'title': 'Article three',
+            'body': 'Bacon',
+            'author': 'Mark',
+            'create_date': '29-12-2017'
+
+        }
+
+
+
+
+    ]
+    return articles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
